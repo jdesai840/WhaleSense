0768b96b46f22b228b58586e6bca3f6496358a9d
jdesai840 <69543549+jdesai840@users.noreply.github.com>
2022-04-01 04:10:59 -0400

    Update GPSmodule.ino

--- GPS/GPSmodule.ino
+++ GPS/GPSmodule.ino
@@ -2,14 +2,22 @@
 
 #include "TinyGPS++.h"
 #include "SoftwareSerial.h"
+#include "Servo.h"
 
 SoftwareSerial serial_connection(10, 11); //RX=pin 10, TX=pin 11
 TinyGPSPlus gps;
+Servo pitchServo;
+Servo yawServo;
+
 void setup()
 {
+  //Initializing GPS module
   Serial.begin(9600);
   serial_connection.begin(9600);
   Serial.println("GPS Start");
+  //Initializing servo for thermal camera gimbal control
+  pitchServo.attach(8);
+  yawServo.attach(9);
 }
 
 void loop()
@@ -18,7 +26,7 @@ void loop()
   {
     gps.encode(serial_connection.read());
   }
-  if(gps.location.isUpdated())
+  if(gps.location.isUpdated()) //Collecting GPS data, and sending it to Raspi via USB Serial
   {
     Serial.println("Satellite Count:");
     Serial.println(gps.satellites.value());
