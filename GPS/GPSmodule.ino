8495ee16fbc2f5b5fe38f84b43da6b9c3000f749
jdesai840 <69543549+jdesai840@users.noreply.github.com>
2022-04-01 01:41:54 -0400

    Update GPSmodule.ino

--- GPS/GPSmodule.ino
+++ GPS/GPSmodule.ino
@@ -1,1 +1,36 @@
+//This code allows the Arduino microcontroller to read GPS data, and then send it to the Raspberry Pi via USB serial communication
+
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
+
+SoftwareSerial serial_connection(10, 11); //RX=pin 10, TX=pin 11
+TinyGPSPlus gps;
+void setup()
+{
+  Serial.begin(9600);
+  serial_connection.begin(9600);
+  Serial.println("GPS Start");
+}
+
+void loop()
+{
+  while(serial_connection.available())
+  {
+    gps.encode(serial_connection.read());
+  }
+  if(gps.location.isUpdated())
+  {
+    Serial.println("Satellite Count:");
+    Serial.println(gps.satellites.value());
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    Serial.println("Speed MPH:");
+    Serial.println(gps.speed.mph());
+    Serial.println("Altitude Feet:");
+    Serial.println(gps.altitude.feet());
+    Serial.println("");
+  }
+}
 
